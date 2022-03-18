5a7b590ba8219b1de7e2e1c9bdcc679ba6064fa4
Aegi04 <egigunawan035@gmail.com>
2022-03-18 23:37:15 +0700

    sintaksis data list comprehension

--- sintaksis-dasar-data-list-comprehension.py
+++ sintaksis-dasar-data-list-comprehension.py
@@ -3,7 +3,7 @@ Sintaksis dasar dalam bentuk data list comprehension
 """
 
 print('daftar buku :')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 print(daftar_buku)
 
 print('\nperintah del simple pada elemen ke-2')
@@ -19,14 +19,14 @@ for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\ndi urutan elemen ke [0:3]')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 del daftar_buku[0:3] #start:end
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-print('\ndi urutan elemen ke [0:-2]')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
-del daftar_buku[0:-2] #start:end
+print('\ndi urutan elemen ke [0:-3]')
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
+del daftar_buku[0:-3] #start:end
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
@@ -34,13 +34,13 @@ for i in range(0, len(daftar_buku)):
 
 print('\nperintah del dengan data list comprehension #START:END')
 print('\ndi urutan elemen ke [0::2]')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 del daftar_buku[0::2] #start:end:step
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\ndi urutan elemen ke [0::-1]')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 del daftar_buku[0::-1] #start:end:step
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
@@ -50,7 +50,7 @@ for i in range(0, len(daftar_buku)):
 print('\nperintah del dengan data list comprehension')
 
 print('\nmembuat list baru yang sudah di del')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 
 daftar_buku_baru = daftar_buku[:]
 del daftar_buku[:]
@@ -63,25 +63,25 @@ for i in range(0, len(daftar_buku_baru)):
 # Perintah del dengan list comprehension GANJIL:GENAP
 
 print('\nperintah del dengan list comprehension #GANJIL')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 del daftar_buku[0::2] #start:stop:end
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\nperintah del dengan list comprehension #GENAP')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 del daftar_buku[1::2] #start:stop:end
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\nperintah del dengan list comprehension #buang di ujung tertentu')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 del daftar_buku[0:-1:2] #start:stop:end
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
 print('\nperintah del dengan list comprehension #tanpa for in')
-daftar_buku = ['seven habits', 'how to influence people', 'first thing fist', '4dx']
+daftar_buku = ['1. seven habits', '2. how to influence people', '3. first thing fist', '4. 4dx']
 print(daftar_buku[0::2])
 print(daftar_buku[1::2])
 print(daftar_buku[0:-1:2])
